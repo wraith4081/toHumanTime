5c21d36406ae7f838ce5d4a2ff287950e8b2d6e6
wraith4081 <54374743+wraith4081@users.noreply.github.com>
2023-07-21 04:46:53 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,31 +2,13 @@ const floor = (num: number) => ~~num;
 
 export default function fancyTimeFormat(duration: number)
 { 
-  if (duration > 185542587187199) return null;
-  
-  if (isNaN(parseInt(duration)) || duration <= 0) return null;
-  
-  const timePeriods = [
-    'day',
-    'hour',
-    'minute',
-    'second'
-  ];
+  if (duration > 185542587187199 || isNaN(parseInt(duration)) || duration <= 0) return null;
+
   const str = [
-    floor(
-        duration / 86400
-    ), 
-    floor(
-        (duration % 86400) / 3600
-    ), 
-    floor(
-        (duration % 3600) / 60
-    ), 
-    floor(duration % 60)
-  ].map((e, i) => {
-    if (e <= 0) return '';
-    return `${String(e).padStart(2, '0')} ${timePeriods[i] + (e > 1 ? 's' : '')}`
-  }).filter(String);
-  if (str.length == 1) return str[0];
-  return str.slice(0, -1).join(', ') + ' and ' + str.slice(-1);
+    ['day', floor(duration / 86400)], 
+    ['hour', floor((duration % 86400) / 3600)], 
+    ['minute', floor((duration % 3600) / 60)], 
+    ['second', floor(duration % 60)]
+  ].map(([name, time], i) => time <= 0 ? '' : `${String(time).padStart(2, '0')} ${name + (time > 1 ? 's' : '')}`).filter(String);
+  return (str.length == 1) ? str[0] : str.slice(0, -1).join(', ') + ' and ' + str.slice(-1);
 }
